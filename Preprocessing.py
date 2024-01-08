799186b76dcdbc8cdf22916127ad4db9208e3824
Niccoló Barbieri - niccolo.barbieri@studio.unibo.it <STUDENTI\niccolo.barbieri@str957-bio10.personale.dir.unibo.it>
2024-01-08 13:09:44 +0100

    Documentation

--- Preprocessing.py
+++ Preprocessing.py
@@ -10,8 +10,9 @@ Preprocessing:
     Map labels to numerical values.
 Train-Test-Validation Split:
     Split the preprocessed data into training, testing, and validation sets
+"""
 
-"""import pandas as pd
+import pandas as pd
 import numpy as np
 import seaborn as sns
 import json
@@ -20,8 +21,18 @@ from DIRS import TRANSFORMERS_CACHE_DIR, DATA_DIR, LARGE_DATA_DIR
 labels=['ProVax','AntiVax','Neutral']
 random_state=42
 
-def undersampling(df, random_state):
-    # undersampling function, since label classes are unbalancd (ProVax is the smallest)
+def (df: pd.DataFrame, random_state: int) -> pd.DataFrame:
+    """
+    Perform undersampling on a DataFrame to balance label classes.
+
+    Parameters:
+    - df (pd.DataFrame): The input DataFrame containing a 'label' column.
+    - random_state (int): Seed for reproducibility in random sampling.
+
+    Returns:
+    - pd.DataFrame: Undersampled DataFrame with balanced label classes.
+
+    """
     l = df.label.value_counts()['ProVax']
     out = pd.concat([
         df[df.label == 'ProVax'],
@@ -30,7 +41,31 @@ def undersampling(df, random_state):
     ], ignore_index=False)
     return out
 
-def reading_merging(path_df,name_df,dtype_df,path_com,names_com,dtype_com,path_pos,name_pos):
+def reading_merging(path_df: str,
+    name_df: list,
+    dtype_df: dict,
+    path_com: str,
+    names_com: list,
+    dtype_com: dict,
+    path_pos: str,
+    name_pos: list
+) -> pd.DataFrame:
+    """
+    Read and merge data from multiple sources into a single DataFrame.
+
+    Parameters:
+    - path_df (str): Filepath for the main DataFrame CSV file.
+    - name_df (list): Column names for the main DataFrame.
+    - dtype_df (dict): Data types for columns in the main DataFrame.
+    - path_com (str): Filepath for the community DataFrame CSV file.
+    - names_com (list): Column names for the community DataFrame.
+    - dtype_com (dict): Data types for columns in the community DataFrame.
+    - path_pos (str): Filepath for the JSON file containing position data.
+    - name_pos (list): Column names for the position DataFrame.
+
+    Returns:
+    - pd.DataFrame: Merged DataFrame containing data from the main DataFrame, community DataFrame, and position data.
+    """
     df = pd.read_csv(
         path_df,
         index_col="id",
@@ -56,7 +91,27 @@ def reading_merging(path_df,name_df,dtype_df,path_com,names_com,dtype_com,path_p
     df=df.merge(df_pos,how="left",left_on="user.id",right_index=True)
     return df
 
-def preproc(df,label,seed):
+def preproc(    df: pd.DataFrame,
+    label: list,
+    seed: int
+) -> Tuple[pd.DataFrame, np.ndarray]:
+    """
+    Preprocess DataFrame for text classification.
+    Missing values in the text will be replaced, unused columns will be removed, and used columns will be renamed.
+    Type checking for the columns (e.g leiden_90 and louvain_90 are fixed to int).
+    Label column are mapped to int.
+    Undersampling is applied.
+    
+
+    Parameters:
+    - df (pd.DataFrame): Input DataFrame containing text data and annotations.
+    - label (list): List of labels for classification.
+    - seed (int): Random seed for undersampling.
+
+    Returns:
+    - Tuple[pd.DataFrame, np.ndarray]: Tuple containing the preprocessed DataFrame and corresponding indices.
+
+    """
     df_anno=df[df['annotation'].notna()]
     df_anno.loc[:,'text']=df_anno['text'].apply(lambda x: x.replace('\n',' ').replace('\t','').replace("\r\n"," ").replace('\u0085'," ").replace('\u2028'," ").replace('\u2029'," "))
     df_anno=df_anno[['text','annotation',"leiden_90","louvain_90","x_pos","y_pos"]].rename(columns=
@@ -77,7 +132,8 @@ def preproc(df,label,seed):
     df_anno["label"]=df_anno["label"].apply(int)
     return (df_anno,ids)
 
-def main(DATA_INFO):
+def main(DATA_INFO: Tuple):
+    """Do the main"""
     path_df,name_df,dtype_df,path_com,names_com,dtype_com,path_pos,name_pos,seed,label,DATA_PATH=DATA_INFO
     df,ids=preproc(reading_merging(path_df,name_df,dtype_df,path_com,names_com,dtype_com,path_pos,name_pos,dtype_pos),label,seed)
     id_train,id_test=train_test_split(ids, test_size=0.33, random_state=42)
