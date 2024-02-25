fbd6328dfec4f1ce8e6439294e6bea0fe95f7d4c
Niccolò Barbieri <75387092+Niconiki99@users.noreply.github.com>
2024-02-25 13:44:45 +0100

    Delete DIRS.py
    Removing old version of configuration after adding the .txt version

--- DIRS.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-p=pathlib.Path()
-this=p.cwd()
-TRANSFORMERS_CACHE_DIR=str(this/"Trasf_cache")
-DATA_DIR=str(this/"Data/Data/")
-LARGE_DATA_DIR=str(this/"Data/Large_Data/")
-NETWORK_DATA=str(this/"Data/Network_Data")
-TEST_PATH=str(this/"Data/Test")
